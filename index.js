1a0fbeec906a1f4516b72f1a5a2ee5b36255357c
silvercho <tjddms01212@gmail.com>
2024-05-02 12:23:38 +0900

    express

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-var figlet = require("figlet");
+const express = require("express");
+const app = express();
 
-figlet("JoeunLog", function (err, data) {
-  if (err) {
-    console.log("Something went wrong...");
-    console.dir(err);
-    return;
-  }
-  console.log(data);
+app.get("/", function (req, res) {
+  res.send("Hello World");
 });
+app.get("/user/:id", (req, res) => {
+  const q = req.params;
+  console.log(q);
+  res.send({ sound: "안녕" });
+});
+
+app.get("/sound/:name", (req, res) => {
+  const { name } = req.params;
+  if (name == "dog") {
+    res.json({ sound: "멍멍" });
+  } else if (name == "cat") {
+    res.json({ sound: "야옹" });
+  } else if (name == "pig") {
+    res.json({ sound: "꿀꿀" });
+  } else {
+    res.json({ sound: "알수없음" });
+  }
+});
+
+app.listen(3000);
