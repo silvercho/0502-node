baeb3df43838560b9789b2529018e85f320acae4
silvercho <tjddms01212@gmail.com>
2024-05-02 11:10:57 +0900

    figlet

---
+++ index.js
@@ -0,0 +1,10 @@
+var figlet = require("figlet");
+
+figlet("JoeunLog", function (err, data) {
+  if (err) {
+    console.log("Something went wrong...");
+    console.dir(err);
+    return;
+  }
+  console.log(data);
+});
